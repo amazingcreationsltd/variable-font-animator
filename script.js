31e221048f69b29ee063bb41f9c87c756258588a
Debanga Dutta <71249161+amazingcreationsltd@users.noreply.github.com>
2025-04-13 00:06:09 +0530

    script.js

--- script.js
+++ script.js
@@ -1,12 +1,24 @@
-        // Wait for DOM to be fully loaded
-        $(document).ready(function() {
-            var theText = "Variable fonts are awesome!";
-            $(".text-animation-container").each(function() {
-                for (let i = 0; i < theText.length; i++) {
-                    let delay = (i * 1.5) / theText.length;
-                    let e = $("<span>" + theText[i] + "</span>");
-                    $(this).append(e);
-                    e.css("animation-delay", delay + "s");
-                }
-            });
-        }); 
+$(document).ready(function () {
+  var theText = "Variable fonts are awesome!";
+  $(".text-animation-container").each(function () {
+    // Clear any existing content
+    $(this).empty();
+
+    // Create a wrapper div for better word wrapping
+    var wrapper = $("<div class='text-wrapper'></div>");
+    $(this).append(wrapper);
+
+    for (let i = 0; i < theText.length; i++) {
+      let delay = (i * 1.5) / theText.length;
+      let character = theText[i];
+      let e = $("<span>" + character + "</span>");
+      wrapper.append(e);
+      e.css("animation-delay", delay + "s");
+
+      // Add a non-breaking space after each word to ensure proper word breaks
+      if (character === " ") {
+        e.html("&nbsp;");
+      }
+    }
+  });
+});
